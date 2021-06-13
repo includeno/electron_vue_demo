efbb10fe9b397ea711925408e55e666cb466aae0
cdli_i <cdli_i@didiglobal.com>
2021-06-13 10:53:05 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,23 @@
+
+
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "eleapp",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+/* CSS */
+</style>
